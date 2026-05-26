2d81a76cd01213a6ee78c350f15773e8b359ade2
TerenceP1 <158529801+TerenceP1@users.noreply.github.com>
2026-05-26 17:58:21 -0400

    Update build.bat to remove opencv4 package
    Removed opencv4 package after building.

--- build.bat
+++ build.bat
@@ -3,6 +3,6 @@ call compile.bat
 cd ..
 powershell -Command "Start-BitsTransfer -Source https://github.com/opencv/opencv/releases/download/4.13.0/opencv-4.13.0-windows.exe -Destination opencv.exe"
 opencv.exe -oopencv -y
-
+vcpkg remove opencv4:x64-windows
 cmake . -DCMAKE_TOOLCHAIN_FILE=vcpkg\scripts\buildsystems\vcpkg.cmake  -DVCPKG_TARGET_TRIPLET=x64-windows
 cmake --build . --config Release
