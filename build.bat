4753c5b864422613507995af7c19e43f8ae0f9cf
TerenceP1 <158529801+TerenceP1@users.noreply.github.com>
2026-05-26 18:01:03 -0400

    Refactor build.bat to include additional vcpkg removals

--- build.bat
+++ build.bat
@@ -3,6 +3,10 @@ call compile.bat
 cd ..
 powershell -Command "Start-BitsTransfer -Source https://github.com/opencv/opencv/releases/download/4.13.0/opencv-4.13.0-windows.exe -Destination opencv.exe"
 opencv.exe -oopencv -y
+vcpkg remove opencv:x64-windows
+vcpkg remove opencv1:x64-windows
+vcpkg remove opencv2:x64-windows
+vcpkg remove opencv3:x64-windows
 vcpkg remove opencv4:x64-windows
 cmake . -DCMAKE_TOOLCHAIN_FILE=vcpkg\scripts\buildsystems\vcpkg.cmake  -DVCPKG_TARGET_TRIPLET=x64-windows
 cmake --build . --config Release
