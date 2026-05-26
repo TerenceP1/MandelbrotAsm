61c8db1f716e27b8571f0af904fc69f622364e5b
TerenceP1 <158529801+TerenceP1@users.noreply.github.com>
2026-05-26 17:14:01 -0400

    Modify build.bat for CMake toolchain and triplet
    Updated CMake command to include toolchain file and target triplet.

--- build.bat
+++ build.bat
@@ -4,5 +4,5 @@ cd ..
 powershell -Command "Start-BitsTransfer -Source https://github.com/opencv/opencv/releases/download/4.13.0/opencv-4.13.0-windows.exe -Destination opencv.exe"
 opencv.exe -oopencv -y
 
-cmake .
+cmake . -DCMAKE_TOOLCHAIN_FILE=${{ github.workspace }}\vcpkg\scripts\buildsystems\vcpkg.cmake  -DVCPKG_TARGET_TRIPLET=x64-windows
 cmake --build . --config Release
