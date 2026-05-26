234a7b83a7bca8c4c29b94847414f03d5ca57436
TerenceP1 <158529801+TerenceP1@users.noreply.github.com>
2026-05-26 18:53:09 -0400

    Modify build.bat to update OpenCV version
    Updated OpenCV version in project configuration and build script.

--- build.bat
+++ build.bat
@@ -9,4 +9,5 @@ vcpkg remove opencv2:x64-windows
 vcpkg remove opencv3:x64-windows
 vcpkg remove opencv4:x64-windows
 cmake . -DCMAKE_TOOLCHAIN_FILE=C:\vcpkg\scripts\buildsystems\vcpkg.cmake  -DVCPKG_TARGET_TRIPLET=x64-windows
+powershell -Command "(Get-Content 'MandelbrotAsm\MandelbrotAsm\MandelbrotAsm.vcxproj' -Raw) -replace 'opencv_world4120\.lib','opencv_world4130.lib' | Set-Content 'MandelbrotAsm\MandelbrotAsm\MandelbrotAsm.vcxproj'"
 cmake --build . --config Release
