48441a97aeffedc59805e07f9eb1ed3791a3f3ee
TerenceP1 <158529801+TerenceP1@users.noreply.github.com>
2026-05-26 20:09:29 -0400

    Update build.bat for OpenCV 4.13.0 integration

--- build.bat
+++ build.bat
@@ -9,5 +9,8 @@ vcpkg remove opencv2:x64-windows
 vcpkg remove opencv3:x64-windows
 vcpkg remove opencv4:x64-windows
 cmake . -DCMAKE_TOOLCHAIN_FILE=C:\vcpkg\scripts\buildsystems\vcpkg.cmake  -DVCPKG_TARGET_TRIPLET=x64-windows
+echo =============
+type CMakeCache
+echo =============
 powershell -Command "(Get-Content 'MandelbrotAsm.vcxproj' -Raw) -replace 'opencv_world4120\.lib','opencv_world4130.lib' | Set-Content 'MandelbrotAsm.vcxproj'"
 cmake --build . --config Release
